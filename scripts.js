6b416100838838ac5a3ccba1038e83ee3b5dfdf9
Gabriel Medeiros <gabrielmedeirosbiel518@gmail.com>
2024-07-25 18:20:11 -0300

    Projeto tesla concluido

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector('.formulario')
+
+function aparecer (){
+    formulario.style.left = "55%"
+}
+
+function desaparecer (){
+    formulario.style.left = "-55%"
+}
